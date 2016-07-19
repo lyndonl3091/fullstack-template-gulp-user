d3ccaa65c098f1fd6eb0925424100c2ef1b92bfd
lyndonl3091 <lyndonl3091@gmail.com>
2016-07-19 07:58:10 -0700

    added more

--- client/js/controllers/controllers.js
+++ client/js/controllers/controllers.js
@@ -22,17 +22,43 @@ app.controller('mainCtrl', function() {
         console.log('err:', err);
       })
   };
-});
-
 
 });
 
 app.controller('loginCtrl', function() {
   console.log('loginCtrl!');
 
+  $scope.userLogin = () => {
+      $auth.login($scope.user)
+      .then(res => {
+        $state.go('profile')
+      })
+      .catch(err => {
+        console.log('err:', err);
+      })
+    }
 
-
+    $scope.register = () => {
+      if($scope.newUser.password !== $scope.newUser.password2) {
+        $scope.newUser.password = null;
+        $scope.newUser.password2 = null;
+        alert('Passwords must match.  Try again.')
+      } else {
+        console.log('$scope.newUser', $scope.newUser)
+        $auth.signup($scope.newUser)
+        .then(res => {
+          $auth.login($scope.newUser)
+          .then(res => {
+            $state.go('profile')
+          })
+        })
+        .catch(err => {
+          console.log('err:', err);
+        })
+      }
+    }
 });
+
 
 app.controller('profileCtrl', function() {
   console.log('profileCtrl!');
