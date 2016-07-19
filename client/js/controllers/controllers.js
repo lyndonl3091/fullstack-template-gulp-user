a7590dc70456b741232722f3badcc95cfaafd91b
lyndonl3091 <lyndonl3091@gmail.com>
2016-07-19 07:49:47 -0700

    added more

--- client/js/controllers/controllers.js
+++ client/js/controllers/controllers.js
@@ -5,3 +5,11 @@ var app = angular.module('myApp');
 app.controller('mainCtrl', function() {
   console.log('mainCtrl!');
 });
+
+app.controller('loginCtrl', function() {
+  console.log('loginCtrl!');
+});
+
+app.controller('profileCtrl', function() {
+  console.log('profileCtrl!');
+});
--- client/js/module.js
+++ client/js/module.js
@@ -6,6 +6,8 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
     .state('home', { url: '/', templateUrl: '/html/home.html' })
+    .state('login', { url: '/login', templateUrl: '/html/login.html', controller: 'loginCtrl' })
+    .state('home', { url: '/profile', templateUrl: '/html/home.html', controller: 'profileCtrl' })
 
   $urlRouterProvider.otherwise('/');
 });
